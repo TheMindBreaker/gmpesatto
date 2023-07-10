5e0a96b9ecaae31e9945d348fa74dafec2147b24
TheMindBreaker <A01229632@tec.mx>
2023-07-10 17:05:37 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const net = require("net");
+let server = net.createServer();
+
+server.on("connection", (socket) => {
+    socket.setKeepAlive(true);
+    socket.setTimeout(10000);
+    socket.on("data", function (data) {
+        try {
+            let params = JSON.parse(data.toString());
+            console.log(params);
+        } catch(e) {
+            console.log(e)
+        }
+        
+    })
+
+    socket.on("timeout", () => {
+        login.logout(socket.remotePort, socket.remoteAddress, clients[socket.remotePort].id);
+        delete clients[socket.remotePort];
+    })
+
+    socket.on("error", (err) => {})
+})
+
+server.on("error", (err) => {})
+
+
+server.on("listening", () => {
+    log.info("SERVER LISTENING: 3080");
+})
